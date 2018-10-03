ded489d33df01446ffcef1e085da7d28bd4b3202
Darryl J Frost <darryl.jade.frost@gmail.com>
2018-10-03 22:07:14 +0100

    first commit

---
+++ ex1.py
@@ -0,0 +1,5 @@
+print("Hedllo World!")
+print("This is exciting")
+print("Isn't this a thrill?")
+print("I said isn't this a thrill?")
+print("No, you are right, it is boring!")
---
+++ ex4.py
@@ -0,0 +1,21 @@
+# replace these with your own values!
+my_name = 'Chris Janes'
+my_age = 21 # maybe
+my_height = 67 # inches
+my_weight = 160 # pounds
+my_eyes = 'Green'
+my_hair = 'Ginger'
+is_heavy = my_weight > 3000
+to_kilo = my_weight * 0.45 # kilograms
+to_cent = my_height * 2.54 # centimetres
+
+print(f"Let's talk about {my_name}.")
+# swap in your preferred pronoun.
+print(f"He is {my_height} inches tall.")
+print(f"He's {my_weight} pounds heavy.")
+print(f"It is {is_heavy} that he is overweight.")
+print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+total = my_age + my_height + my_weight
+print(f"If I add {my_age}, {my_height} and {my_weight} I get {total}")
+print(f"His weight in kilos is {to_kilo}")
+print(f"His height in centimetres is {to_cent}" )
