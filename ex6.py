a3c5870cbfc79de7f6d297997d6bedce5648a473
Darryl J Frost <darryl.jade.frost@gmail.com>
2018-10-10 15:05:21 +0100

    second commit. trying to include a bmi weight conversion

--- ex6.py
+++ ex6.py
@@ -5,9 +5,19 @@ if(answer) == "no":
     print("I'm sorry to hear that!!")
 if(answer) == "yes":
     print("Great! Lets get started!")
+
 name = input("What is your name?" + " ")
 print("Hi there" + " " + str(name) + " " + "nice to meet you")
+
 age = int(input("How old are you if you dont mind me asking?" + " "))
 if(age <= 18):
     print("Ahh im sorry, our minimum age for this site is 18! Please come back in" +
           " " + str(18-age) + " " + "years!")
+if(age >= 18):
+    print("You are" + " " + str(age) + " " + "years old")
+
+height = int(input("How tall are you in inches?" + " "))
+print("You are" + " " + str(height) + " " + "inches tall. Which is" + " " +
+      str(height*2.54) + " " + "in centimetre's or" + " " + str(height/12) + " " + " in feet if you were interested :)")
+
+weight = int(input("What is your weight in kilograms?" + " "))
