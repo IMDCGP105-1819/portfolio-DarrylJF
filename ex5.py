55a7c325b8aad74afe711f72ccb9aee7d31a8fe3
Darryl J Frost <darryl.jade.frost@gmail.com>
2018-10-10 13:16:31 +0100

    final commit

--- ex5.py
+++ ex5.py
@@ -1,10 +1,14 @@
-beerBottles = 99
-story = (f"{beerBottles} bottles of beer on the wall, {beerBottles} bottles of beer.\nTake one down, pass it around, {beerBottles} bottles of beer on the wall")
+beer = 99
 
-while (beerBottles >= 1):
-    if beerBottles != 0:
-        print(story)
-        beerBottles = beerBottles - 1
-    else:
-        print("You made it!")
-        break
+while (beer > 0):
+    print(str(beer)+" bottles of beer on the wall," +
+          str(beer) + " bottles of beer. ")
+    beer = beer-1
+    print("take one down, pass it around," +
+          str(beer)+" bottles of beer on the wall...")
+    beer = beer - 1
+if (beer == 0):
+    print("more bottles of beer on the wall, no more bottles of beer.")
+    beer = 99
+    print("Go to the store and buy some more," +
+          str(beer) + "bottles of beer on the wall...")
