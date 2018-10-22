dbf5874e14e80cdeaacac7e6f778266baddbff28
DarrylJF <darryl.jade.frost@gmail.com>
2018-10-22 16:06:52 +0100

    trying to get the program to go to a different loop if invalid syntax is entered, once valid answer is entered it continues questions (possible addon, after 5 attempts at answer it prints message and stops program)

--- ex6.py
+++ ex6.py
@@ -1,25 +1,47 @@
-answer = input(
-    "Hi there, If you have a moment i'd like to ask you a few questions is that ok? Type Yes or No:" + " ")
+def answernotvalid(): #answernotvalid is defined so if the user types anything that is not "yes" or "no" it will execute the print statement and the program will return to the beginning looking for a valid answer 
+    answer = input("Invalid syntax, please type Yes or No: ")
+    while answer != ("yes", "no"):
+        answernotvalid()
+    if answer == ("yes"):
+         main()
+    if answer == ("no"):
+        exit()
 
-if(answer) == "no":
-    print("I'm sorry to hear that!!")
-if(answer) == "yes":
-    print("Ok! Lets get started!")
 
-name = input("What is your name?" + " ")
-print("Hi there" + " " + str(name) + " " + "nice to meet you")
+def start(): 
+    
+    answer = input("Hi do you have a minute? ").lower()
+    if answer == ("yes"):
+        print("Great lets get started! ")
+        main()
+    if answer == ("no"):
+        print("I'm sorry to hear that, maybe next time! ")
+        exit()
+    if answer == ("yes" , "no"):
+        answernotvalid()
 
-age = int(input("How old are you if you dont mind me asking?" + " "))
-if(age <= 18):
-    print("Ahh im sorry, our minimum age for this site is 18! Please come back in" +
-          " " + str(18-age) + " " + "years!")
-else:
-    print("You are" + " " + str(age) + " " + "years old")
+def main():
+    name = input("What is your name? ")
+    print(f"Hi there {name} nice to meet you")
 
-height = float(input("How tall are you in inches?" + " "))
-print("You are" + " " + str(height) + " " + "inches tall. Which is" + " " +
-      str(height*2.54) + " " + "in centimetre's or" + " " + str(height/12) + " " + "feet if you were interested :)")
+    age = int(input("How old are you if you dont mind me asking? "))
 
-weight = float(input("What is your weight in kilograms?" + " "))
-print("You weigh" + " " + str(weight) + " " + "Kilograms. Which is" +
-      " " + str(weight*2.205) + " " + "pounds incase you were interested!")
+    if(age < 18):
+        print(f"Ahh im sorry, our minimum age for this site is 18! Please come back in {18-age} years!")
+    else:
+        print(f"You are {age} years old")
+
+    height = float(input("How tall are you in inches? "))
+    print(f"You are {height} inches tall. Which is {height*2.54} in centimetres or {height/12} feet if you were interested")
+
+    weight = float(input("What is your weight in kilograms? "))
+    print(f"You weigh {weight} Kilograms. Which is {weight*2.205} pounds if you were interested!")
+
+    eye_colour = input("What colour are you eyes? ")
+    hair_colour = input("What colour is your hair? ")
+
+    print(f"Thanks for the info {name}. You are {age} years old, {height} inches tall, your total weight (according to you) is {weight} kg , the colour of your eyes are {eye_colour} and your hair is {hair_colour}")
+
+start()
+answernotvalid()
+main()
