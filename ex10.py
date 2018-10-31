2574133a2c836e7558573bad00f183978ecaf4ba
DarrylJF <darryl.jade.frost@gmail.com>
2018-10-31 19:05:37 +0000

    first commit

---
+++ ex10.py
@@ -0,0 +1,12 @@
+count = 0
+
+while count < 100:
+    if count % 3 == 0:
+        print("Fizz")
+    elif count % 5 == 0:
+        print("Buzz")
+    elif count % 3 == 0 and count % 5 == 0:
+        print("FizzBuzz")
+    else:
+        print(count)
+    count += 1
