d1b09555309a8e898c64c5e96059d87c42a8e135
darryljf <darryl.jade.frost@gmail.com>
2018-11-08 11:44:11 +0000

    ammended changes. program now runs but calculations incorrect

--- ex13.py
+++ ex13.py
@@ -3,15 +3,17 @@ def hotel_cost(nights):
     return cost*nights
 
 def plane_ticket_cost(city, klass):
+    flight_cost = 0
+    
     if city.lower == "new york":
-        return 2000
+        flight_cost = 2000
     elif city.lower == "auckland":
-        return 790
+        flight_cost =  790
     elif city.lower == "venice":
-        return 154
+        flight_cost =  154
     elif city.lower == "glasgow":
-        return 65
-    return city + city*klass
+        flight_cost =  65
+    return flight_cost*klass
 
 def rental_car_cost(days):
     cost = days*30
@@ -21,25 +23,28 @@ def rental_car_cost(days):
         cost -=30
     return cost
 
-def total_cost(city, days):
+def total_cost():
     cost = 0
-    cost += hotel_cost
-    cost += plane_ticket_cost
-
-    print("List of cities to travel: \n", "New York \n", "Auckland \n", "Venice \n", "Glasgow")
-    print("Pick a class: \n", "Economy: 1 \n", "Premium Economy: 1.3 \n", "Business Class: 1.6 \n", "First Class: 1.9 \n", "Please enter the decimal value displayed... ")
-
-    cost += city + city*klass
-
-    print("Car Hire is £30 per day \n", "A £50 discount will be applied for rentals over 7 days \n", "A £30 discount will be applied for 3-6 days")
-    print("Enter how many days you want to hire: ")
-
-    cost += rental_car_cost
     
+   
+    
+    
+    
+    print("List of cities to travel: \n", "New York \n", "Auckland \n", "Venice \n", "Glasgow")
+    city = str(input("Type the city to visit:"))
+    print("Pick a class: \n", "Economy: 1 \n", "Premium Economy: 1.3 \n", "Business Class: 1.6 \n", "First Class: 1.9 \n", "Please enter the decimal value displayed... ")
+    klass = float(input())
+    cost += plane_ticket_cost(city, klass)
+    print("Car Hire is £30 per day \n", "A £50 discount will be applied for rentals over 7 days \n", "A £30 discount will be applied for rentals between 3-6 days")
+    print("Enter how many days you want to hire: ")
+    days = int(input())
+    cost += rental_car_cost(days)
+    
+    cost += hotel_cost(days-1)
+
+
     print("The final cost of your trip is", cost)
 
-days = int(input())
-klass = float(input())
-city = str(input())
-total_cost(city,days)
+    
+total_cost()
 
